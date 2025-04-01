8418384d2f2081091390306fabd44fde31a33f32
mclavelm <mclavelm@LAPTOP-EFVE94I6>
2025-04-01 19:17:02 +0200

    Eliminar archivos no usados

--- htseq_counts.sh
+++
@@ -1,8 +0,0 @@
-#bucle para iterar sobre cada archivo bam
-for bam in ./*.bam; do
-	sample=$(basename "$bam" .bam)
-	htseq-count -f bam -r pos -t exon \
-	"$bam"  ../../gencode.v37lift37.annotation.gtf \
-	> ../counts/hisat2/"${sample}_counts.tsv"
-done 
-
